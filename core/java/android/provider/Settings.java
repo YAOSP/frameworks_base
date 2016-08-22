c9ae6c1961f3ef83cbafd8e98e7f2e275fbecb0a
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-08-22 16:22:27 +0200

    [2/2] frameworks: Lockscreen charging current switch
    Thanx to ezio84 ;)
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3642,6 +3642,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
 
         /**
+         * Whether to show the negociated charger current in the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+
+        /**
          * Network traffic indicator, goes from least to greatest significant bitwise
          * 0 = Display up-stream traffic if set
          * 1 = Display down-stream traffic if set
