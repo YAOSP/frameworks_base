ec4e3626479000f11934b0a18b47dd72499e8fa3
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2016-08-26 13:22:19 +0200

    [2/2] frameworks: ADB over network
    Includes:
    - ADB Over Network, integration of the adb-host mode (already present)
      The feature can be used/tested without the Settings part:
    
        setprop service.adb.tcp.port 5555
    
      Note: This ADB setting is not persistent (for security purpose) and
      require init.rc implementation event like this : http://bit.ly/AdbTcpIP
    
      Author: Tanguy Pruvot
      Id: I5c61a53948349c785356cb5aae165110d75e3074
    
      Author: sssemil <suleymanovemil8@gmail.com>
      Show notification on adb over network too
    
      Screenshots - http://goo.gl/TgsRI6
      Id: I9ddc0aa9a4f330a06ab5d97a8645d1b31bb6f299

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6583,6 +6583,12 @@ public final class Settings {
         public static final String QS_NUM_TILE_COLUMNS = "qs_num_tile_columns";
 
         /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
